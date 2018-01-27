08c01c4d5f3e774bfa5c97a0cbb2ba4ec8279736
Sahil B <sahilb@sahilb.com>
2018-01-27 01:10:44 -0800

    logo and styling changes

--- my-app/src/Row.js
+++ my-app/src/Row.js
@@ -1,31 +1,40 @@
 import React from 'react';
 
-
 export default class Row extends React.Component {
-    constructor() {
-        super()
-        this.state = {
+    splitTitle() {
 
-        }
-    }
-    splitTitle(){
-        var i = 0,
-            len = this.state.post.title.length;
-        var size = 40;
-        var words = words.split(' ');
+        var words = this.props.post.title.split(' ');
+        var lines = [];
+        var prev = ''
+        words.forEach(word => {
+            if (word.length + prev.length > 65) {
+                lines.push(prev);
+                prev = ''
+            } else {
+                prev += (word + ' ');
+            }
+        })
+        lines.push(prev);
+        return lines;
 
-         
     }
     render() {
-        const {post} = this.props;
+        const { post } = this.props;
+        const url =  "https://www.reddit.com" + post.permalink;
         return (
             <div className="row" key={post.id}>
                 <div className="thumbnail row-section">
-                    <img src={post.thumbnail} height={80} width={100} />
+                    <img alt="thumbnail" src={post.thumbnail} height={60} width={80} />
                 </div>
                 <div className="favorite-icon row-section"></div>
-                <div className="row-section">
-                    <div className="title"> {post.title.slice(0,70)} </div>
+                <div className="row-section" >
+                    <a href={url} target="_blank" class="links">
+                    <div className="title">
+                        {this.splitTitle().map(line => {
+                            return <div>{line}</div>
+                        })}
+                    </div>
+                    </a>
                     <div className="comments">
                         {post.num_comments} comments
                     </div>
--- my-app/src/hot.js
+++ my-app/src/hot.js
@@ -1,13 +1,16 @@
-var model =  require('./data.json')
+var model = require('./data.json')
 
-const {data} = model;
-const {children} = data;
+const { data } = model;
+const { children } = data;
 
 const posts = children.map(child => {
-    const {data} = child;
-    const {is_video, media, media_embed, num_comments, title, url, thumbnail, permalink} = data;
-    return {is_video, media, media_embed, num_comments, title, url, thumbnail, permalink}
+    const { data } = child;
+    var { is_video, media, media_embed, num_comments, title, url, thumbnail, permalink } = data;
+    if (thumbnail.indexOf('http') < 0) {
+        thumbnail = 'http://localhost/workspace//reddit_posts/my-app/src/reddit_logo.png';
+    }
+    return { is_video, media, media_embed, num_comments, title, url, thumbnail, permalink }
 })
 const one = posts[0]
 
-export default  {posts, one};
+export default { posts, one };
